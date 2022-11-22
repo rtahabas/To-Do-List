7463a5f9810b72fe7925fde71b752c3111fd7e75
rtahabas <rtahabas@gmail.com>
2022-11-22 20:23:12 +0300

    Craco Config

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+const path = require(`path`);
+
+module.exports = {
+    webpack: {
+        alias: {
+            "@components": path.resolve(__dirname, "src/components"),
+            "@features": path.resolve(__dirname, "src/features"),
+        },
+    },
+};
