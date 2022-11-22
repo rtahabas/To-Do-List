b47778850cff436f94e247ba3eb62826788d65e9
rtahabas <rtahabas@gmail.com>
2022-11-22 21:47:53 +0300

    MUI Added

--- src/features/root/Root.js
+++ src/features/root/Root.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Button from '@mui/material/Button';
 
 const Root = () => {
     return (
         <div>
+            <div>
+                <Button variant="contained">Hello World</Button>
+            </div>
             <Header />
             <Outlet />
             <Footer />
